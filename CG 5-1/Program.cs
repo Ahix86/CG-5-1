1b10103d2b35feff8e1908d697285bb45f36ba25
Allison <allison.hix86@gmail.com>
2018-08-13 18:22:02 -0500

    Homework

--- CG 5-1/Program.cs
+++ CG 5-1/Program.cs
@@ -6,7 +6,32 @@ namespace CG_5_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            {
+                // ask user for first name and assign to variable
+                Console.WriteLine("Please enter your first name");
+                string FirstName = Console.ReadLine();
+
+
+                // ask user for last name
+                Console.WriteLine("Please enter your last name");
+                string LastName = Console.ReadLine();
+
+                //create variable that combine two use inputs into one 'item'
+                string FullName = (FirstName + " " + LastName);
+
+                // Put output together using the two item variable
+                Console.WriteLine($"Hi, {FullName}! Nice to meet you!");
+                Console.ReadLine();
+
+            }
+        }
+        // setting the method to call on in the above output
+        private static string FullName(string FirstName, string LastName)
+        {
+            string FullName = FirstName + " " + LastName;
+
+            return FullName;
         }
     }
 }
